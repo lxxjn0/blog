f9b33c869a05b764d280a93e08bc2e92bbf32023
lxxjn0 <ljy3510@gmail.com>
2022-04-18 01:59:07 +0900

    refactor: logo 컴포넌트 LayoutWrapper에서 제거

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -15,9 +14,6 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="title mono-type hidden text-2xl font-semibold hover:text-primary-600 dark:hover:text-primary-400 sm:block">
                     {siteMetadata.headerTitle}
