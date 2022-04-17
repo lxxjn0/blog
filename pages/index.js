77787e7c1619f4ce375ad5e51dcd2a12c59c3f0d
lxxjn0 <ljy3510@gmail.com>
2022-04-18 02:16:00 +0900

    refactor: Latest 위 padding 추가

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div className="space-y-2 pt-24 pb-8 md:space-y-5">
           <h1 className="mono-type text-2xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             LATEST
           </h1>
